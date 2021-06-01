d1895842d540af5e729349702678b81f8cc1ddab
Jie Ni <nijie321@outlook.com>
2021-06-01 11:51:08 -0500

    Update nmcli_eduroam.sh

--- wifi-scripts/nmcli_eduroam.sh
+++ wifi-scripts/nmcli_eduroam.sh
@@ -3,3 +3,15 @@
 nmcli connection add type wifi con-name eduroam ifname <INTERFACE_NAME> ssid "eduroam" -- wifi-sec.key-mgmt wpa-eap 802-1x.eap ttls 802-1x.identity <EMAIL> 802-1x.phase2-auth mschapv2 
 
 nmcli connection up eduroam --ask
+
+
+nmcli con add \
+  type wifi \
+  con-name eduroam \
+  ifname <INTERFACE_NAME> \
+  ssid "eduroam" \
+  wifi-sec.key-mgmt wpa-eap \
+  802-1x.identity <EMAIL> \
+  802-1x.phase2-auth mschapv2
+  
+ nmcli connection up eduroam --ask

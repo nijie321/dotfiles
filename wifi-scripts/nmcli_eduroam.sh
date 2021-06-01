aa05148b63068b8b5fc65f961eb7300cdcd66c00
Jie Ni <nijie321@outlook.com>
2021-06-01 09:53:19 -0500

    wifi script

---
+++ wifi-scripts/nmcli_eduroam.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+nmcli connection add type wifi con-name eduroam ifname <INTERFACE_NAME> ssid "eduroam" -- wifi-sec.key-mgmt wpa-eap 802-1x.eap ttls 802-1x.identity <EMAIL> 802-1x.phase2-auth mschapv2 
+
+nmcli connection up eduroam --ask

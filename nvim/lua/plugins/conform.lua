7df47d40ae1757ebfc08270f21feeb65cb655c04
Jet <nijie321@outlook.com>
2025-04-11 08:40:34 -0500

    adding treesitter and configure auto-formatting on save

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -1,11 +1,20 @@
 return {
   "stevearc/conform.nvim",
-  event = {"BufWritePre"},
+  event = { "BufWritePre" },
   config = function()
     require("conform").setup({
       formatters_by_ft = {
-        python = {"black"},
-        rust = {"rustfmt"}
+        javascript = { "prettier" },
+        typescript = { "prettier" },
+        javascriptreact = { "prettier" },
+        typescriptreact = { "prettier" },
+        css = { "prettier" },
+        html = { "prettier" },
+        json = { "prettier" },
+        yaml = { "prettier" },
+        markdown = { "prettier" },
+        python = { "isort", "black" },
+        rust = { "rustfmt" }
       },
       format_on_save = {
         timeout_ms = 500,

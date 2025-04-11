5e0193bec5e66767dff953bbe14396b739f018e4
Jet <nijie321@outlook.com>
2025-04-11 11:20:12 -0500

    use lazynvim setup for cmp and add toggleterm

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -2,54 +2,48 @@ return {
   {
     "hrsh7th/nvim-cmp",
     dependencies = {
-      "hrsh7th/cmp-nvim-lsp",     -- LSP completions
-      "hrsh7th/cmp-buffer",       -- Buffer words
-      "hrsh7th/cmp-path",         -- Filesystem paths
-      "saadparwaiz1/cmp_luasnip", -- Snippet completions
-      "onsails/lspkind.nvim",     -- Icons in completion menu
+      "hrsh7th/cmp-nvim-lsp", -- LSP completions
+      "hrsh7th/cmp-buffer",   -- Buffer words
+      "hrsh7th/cmp-path",     -- Filesystem paths
+      -- "saadparwaiz1/cmp_luasnip", -- Snippet completions
+      {
+        "L3MON4D3/LuaSnip",
+        version = "v2.*",
+        -- install jsregexp (optional!).
+        build = "make install_jsregexp",
+      },
+      "onsails/lspkind.nvim", -- Icons in completion menu
     },
-    opts = function(_, opts)
+    config = function()
       local cmp = require("cmp")
       local lspkind = require("lspkind")
+      local luasnip = require("luasnip")
 
-      -- Override default options
-      opts.sources = cmp.config.sources({
-        { name = "nvim_lsp" },
-        { name = "luasnip" },
-        { name = "buffer" },
-        { name = "path" },
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            luasnip.lsp_expand(args.body)
+          end,
+        },
+
+        mapping = cmp.mapping.preset.insert({
+          ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-f>"] = cmp.mapping.scroll_docs(4),
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<C-e>"] = cmp.mapping.close(),
+          ["<CR>"] = cmp.mapping.confirm({
+            behavior = cmp.ConfirmBehavior.Replace,
+            select = true,
+          }),
+        }),
+
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "luasnip" },
+          { name = "buffer" },
+          { name = "path" },
+        }),
       })
-
-      -- Enable icons in completion menu
-      opts.formatting = {
-        format = lspkind.cmp_format({
-          mode = "symbol_text",
-          maxwidth = 50,
-          ellipsis_char = "...",
-        })
-      }
-
-      -- Super-Tab like behavior
-      opts.mapping = cmp.mapping.preset.insert({
-        ["<C-Space>"] = cmp.mapping.complete(),            -- Trigger completion
-        ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Confirm selection
-        ["<Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_next_item()
-          else
-            fallback()
-          end
-        end, { "i", "s" }),
-        ["<S-Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_prev_item()
-          else
-            fallback()
-          end
-        end, { "i", "s" }),
-      })
-
-      return opts
     end,
   },
 }
